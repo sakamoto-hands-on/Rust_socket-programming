0cb1e3f0d952a78e2c766e7918e925d414dcea6f
sakamoto-hands-on <sakamoto.hands.on@gmail.com>
2019-10-23 17:44:50 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use std;;env;
+#[macro_use]
+extern crate log;
+
+mod tcp_client;
+mod tcp_server;
+mod udp_client;
+mod udp_server;
+
+fn main() {
+	env::set\var("RUST_LOG", "debug");
+	env_logger::init();
+	let args:Vec<String>=env::args().collect();
+	if args.len()!=4{
+		error!("Please specify [tcp|udp][server|client][addr:port].");
+		std::process::exit(1);
+	}
+	let protcol: &str=&args[1];
+	let role: &str=&args[2];
+	let address=&args[3];
+	match protocol{
+		"tcp"=>match role{
+			"server"=>{
+				//TODO
+			}
+			"client"=>{
+				//TODO
+			}
+			_=>{
+				missing_role();
+			}
+		},
+		"udp"=>match role{
+			"server"=>{
+				//TODO
+			}
+			"client"=>{
+				//TODO
+			}
+			_=>{
+				missing_role();
+			}
+		},
+		_=>{
+			error!("Please specify tcp or udp on the 1st argument.");
+			std::process::exit(1);
+		}
+	}
+}
+
+fn missing_role(){
+        error!("Please specify tcp or udp on the 2nd argument.");
+	std::process::exit(1);
+}
+	
+
+
+
+}
