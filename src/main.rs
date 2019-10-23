d0b22c175256f884fcf1d5ce68e3e11df3b7d307
sakamoto-hands-on <sakamoto.hands.on@gmail.com>
2019-10-23 19:16:15 +0900

    add tcp_server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std;;env;
+use std::env;
 #[macro_use]
 extern crate log;
 
@@ -8,20 +8,20 @@ mod udp_client;
 mod udp_server;
 
 fn main() {
-	env::set\var("RUST_LOG", "debug");
+env::set_var("RUST_LOG", "debug");
 	env_logger::init();
 	let args:Vec<String>=env::args().collect();
 	if args.len()!=4{
 		error!("Please specify [tcp|udp][server|client][addr:port].");
 		std::process::exit(1);
 	}
-	let protcol: &str=&args[1];
+	let protocol: &str=&args[1];
 	let role: &str=&args[2];
 	let address=&args[3];
 	match protocol{
 		"tcp"=>match role{
 			"server"=>{
-				//TODO
+				tcp_server::serve(address).unwrap_or_else(|e|error!("{}",e));
 			}
 			"client"=>{
 				//TODO
@@ -56,4 +56,4 @@ fn missing_role(){
 
 
 
-}
+
